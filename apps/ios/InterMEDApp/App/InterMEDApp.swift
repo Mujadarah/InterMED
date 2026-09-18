2b43368f0e1621c35e4198d79c53d95814b2b907
Mujadarah <rabiazoabi@gmail.com>
2026-09-18 18:33:16 +0300

    feat: add local-first app configuration

--- apps/ios/InterMEDApp/App/InterMEDApp.swift
+++ apps/ios/InterMEDApp/App/InterMEDApp.swift
@@ -1,10 +1,13 @@
+import InterMEDApplication
 import SwiftUI
 
 @main
 struct InterMEDApp: App {
+    private let configuration = AppConfiguration.production
+
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            HomeView(configuration: configuration)
         }
     }
 }
